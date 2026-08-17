42f2cb5989bfbc6c6009cc6ceea9cd493baad421
KatlehoSephoko <83125789+KatlehoSephoko@users.noreply.github.com>
2026-08-17 22:13:20 +0200

    Create jest.config.js

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testMatch: ['**/?(*.)+(spec|test).ts'],
+  clearMocks: true,
+};
