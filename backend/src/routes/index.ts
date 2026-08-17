7e0aaef182877276bdca9dfb6c1d8a5334857543
KatlehoSephoko <83125789+KatlehoSephoko@users.noreply.github.com>
2026-08-17 21:14:56 +0200

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import healthRoutes from '../modules/health/health.routes';
 import authRoutes from '../modules/auth/auth.routes';
 import driverRoutes from '../modules/drivers/driver.routes';
+import rideRoutes from '../modules/rides/ride.routes';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/', healthRoutes);
 // V1 Modules
 router.use('/auth', authRoutes);
 router.use('/drivers', driverRoutes);
+router.use('/rides', rideRoutes);
 
 export default router;
